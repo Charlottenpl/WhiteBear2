b7f6ba78e58aa795fecef13a1fcdac61666734f4
chenzhongyou <chenzhongyou@topjoy.com>
2022-12-11 23:32:16 +0800

    fix bug

--- app/src/main/java/com/sky/whitebear/view/MainActivity.kt
+++ app/src/main/java/com/sky/whitebear/view/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.sky.whitebear.view
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.sky.whitebear.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.statusBarColor = Color.TRANSPARENT
+        setLightStatusBar()
+    }
+
+    private fun setLightStatusBar() {
+        val flags = window.decorView.systemUiVisibility
+        window.decorView.systemUiVisibility = flags or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+    }
+
+    private fun setDarkStatusBar() {
+        val flags = window.decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        window.decorView.systemUiVisibility = flags xor View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
     }
 }
